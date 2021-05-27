85ba3b3c51fe1d959b42cd628de94d032387e67a
mathiasfc <mathias.falci@gmail.com>
2021-05-27 18:21:16 -0300

    :sparkles: :wrench: added pwa support

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
-module.exports = {
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+const prod = process.env.NODE_ENV === 'production';
+
+module.exports = withPWA({
+  pwa: {
+    disable: !prod,
+    dest: 'public',
+    runtimeCaching
+  },
+
   trailingSlash: true
-};
+});
