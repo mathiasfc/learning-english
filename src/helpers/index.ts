46ebdea52a98876d934693f0f0a81a9208730c1d
mathiasfc <mathias.falci@gmail.com>
2021-06-01 09:03:48 -0300

    :fire: removal of unused helpers

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -3,35 +3,18 @@ import Router from 'next/router';
 import { routes } from './routes';
 
 /**
- * Check if value is a valid e-mail.
- * @param { string } value to be validated
- * @returns { boolean } if the the value is valid e-mail or not
+ * Loads the next word to be rendered
+ * @returns { string } The next random word
  */
-export const isEmail = (value: string) => {
-  // eslint-disable-next-line no-useless-escape
-  const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return regex.test(value);
-};
-
-export const isMobile = (): Boolean => {
-  let isMobileDevice = false;
-
-  if (
-    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-      navigator.userAgent
-    )
-  ) {
-    isMobileDevice = true;
-  }
-
-  return isMobileDevice;
-};
-
 export const loadNextWord = (): string => {
   const randomWord = WORDS[Math.floor(Math.random() * WORDS.length)];
   return randomWord;
 };
 
+/**
+ * Loads the word page, based on word passed by parameter
+ * @param { string } word next valid word
+ */
 export const loadWordPage = (word: string): void => {
   Router.push(`${routes.words.path}${word}`);
 };
